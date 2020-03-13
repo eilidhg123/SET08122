bf5f27cda8dc4e3a509aa7cf1c24f6258cb07312
eilidhg123 <59832371+eilidhg123@users.noreply.github.com>
2020-03-13 17:16:00 +0000

    lab 2 (no errors)

---
+++ lab2.3.c
@@ -0,0 +1,14 @@
+#include < stdio.h >
+
+typedef struct
+{
+int id ;
+char name [30];
+} employee ;
+
+int main ()
+{
+employee e1 = {1 , " Simon "};
+printf ("ID: %d\ nName : %s\n", e1 .id , e1 . name ) ;
+return(0);
+}
